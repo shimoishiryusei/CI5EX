1b2b8158bb5ce3f73682006c4a7476f39d4f15f0
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-16 16:00:53 +0900

    change

--- disp.py
+++ disp.py
@@ -4,7 +4,6 @@ import time
 def set_disp(times):
     Year,Month,Day,Hour,Minute,Second = times
     Time_strings = str(Year)+"/"+str(Month)+"/"+str(Day)+"\n"+str(Hour)+":"+str(Minute)+":"+str(Second)
-    while True:
-        setText(Time_strings)
-        setRGB(0,128,64)
-        time.sleep(1)
+    setText(Time_strings)
+    setRGB(0,128,64)
+    time.sleep(1)
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import alarm
 import get_TH
 import disp
 import get_time
+import time
 
-items = get_time.get_time()
 
 #alarm.main_alarm()
 #get_TH.get_TH()
 
-disp.set_disp(items)
+while True:
+    items = get_time.get_time()
+    disp.set_disp(items)
+    time.sleep(1)
