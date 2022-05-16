361ba4577da81d96676ec04dfe7fd19f7ef66717
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-16 15:57:42 +0900

    add time.sleep

--- disp.py
+++ disp.py
@@ -3,7 +3,7 @@ from grove_rgb_lcd import *
 import time
 def set_disp(times):
     Year,Month,Day,Hour,Minute,Second = times
-    Time_strings = str(Year)+"年"+str(Month)+"月"+str(Day)+"日"+str(Hour)+"時"+str(Minute)+"分"+str(Second)+"秒"
+    Time_strings = str(Year)+"/"+str(Month)+"/"+str(Day)+"\n"+str(Hour)+":"+str(Minute)+":"+str(Second)
     while True:
         setText(Time_strings)
         setRGB(0,128,64)
