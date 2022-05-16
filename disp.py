b92ca6fd539886f556f28033cebac6c88bb4364c
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-16 15:52:58 +0900

    change

--- disp.py
+++ disp.py
@@ -3,6 +3,7 @@ from grove_rgb_lcd import *
 
 def set_disp(times):
     Year,Month,Day,Hour,Minute,Second = times
+    Time_strings = str(Year)+"年"+str(Month)+"月"+str(Day)+"日"+str(Hour)+"時"+str(Minute)+"分"+str(Second)+"秒"
     while True:
-        setText(str(Year)+"年"+str(Month)+"月"+str(Day)+"日"+str(Hour)+"時"+str(Minute)+"分"+str(Second)+"秒")
+        setText(Time_strings)
         setRGB(0,128,64)
