cdd82615a97e83e69724bc13e1043e7c5ee5eb5d
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-16 14:52:41 +0900

    rewrite

---
+++ get_HT.py
@@ -0,0 +1,18 @@
+import grovepi
+import math
+
+sensor = 4
+
+blue = 0
+white = 1
+
+def get_TH():
+    while True:
+        try:
+            [temp,humidity] = grovepi.dht(sensor, blue)
+            if math.isnan(temp) == False and math.isnan(humidity) == False:
+                print("temp = %.02f c humidity = %.02f%%"%(temp, humidity))
+        
+        except IOError:
+            print("Error")
+
