af05328529b21840d9140b824bd7d71eff3086a5
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-16 15:45:53 +0900

    test

--- get_time.py
+++ get_time.py
@@ -1,14 +1,16 @@
 import datetime
-dt_now = datetime.datetime.now()
 
-Year = dt_now.year
-Month = dt_now.month
-Day = dt_now.day
-Hour = dt_now.hour
-Minute = dt_now.minute
-Second = dt_now.second
 
 #print("{}年{}月{}日{}時{}分{}秒".format(Year, Month, Day, Hour, Minute, Second))
 
 def get_time():
+    dt_now = datetime.datetime.now()
+
+    Year = dt_now.year
+    Month = dt_now.month
+    Day = dt_now.day
+    Hour = dt_now.hour
+    Minute = dt_now.minute
+    Second = dt_now.second
+    
     return Year,Month,Day,Hour,Minute,Second
