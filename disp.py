31f6bdc10f71c432a5333d6724ade5c4f99b4154
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-16 15:41:14 +0900

    test

--- disp.py
+++ disp.py
@@ -1,12 +1,8 @@
+from get_time import Year
 from grove_rgb_lcd import *
 
 def set_disp(times):
-    setText("{}年{}月{}日{}時{}分{}秒".format(times[0], times[1], times[2], times[3], times[4], times[5]))
-    setRGB(0,128,64)
-
-    for c in range(0,255):
-        setRGB(c,255-c,0)
-        time.sleep(0.01)
-
-    setRGB(0,255,0)
-    setText("Bye bye, this should wrap")
+    Year,Month,Day,Hour,Minute,Second = times
+    while True:
+        setText(Year+"年"+Month+"月"+Day+"日"+Hour+"時"+Minute+"分"+Second+"秒")
+        setRGB(0,128,64)
