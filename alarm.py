45a4b755bf6e230233212d495549d4446230452d
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-19 16:10:12 +0900

    test

--- alarm.py
+++ alarm.py
@@ -2,22 +2,22 @@ import time
 import grovepi
 
 buzzer = 8
-COUNT_UP = 1
+count_B = 0
 
 grovepi.pinMode(buzzer, "OUTPUT")
 
 def main_alarm(B):
     while True:
         try:
-            if B == 0:
+            if B == 0 & count_B == 0:
                 grovepi.digitalWrite(buzzer,1)
                 print("start")
                 break
 
             elif B == 1:
+                count_B += 1
                 grovepi.digitalWrite(buzzer,0)
                 print("stop")
-                return COUNT_UP
                 break
                 
         except KeyboardInterrupt:
