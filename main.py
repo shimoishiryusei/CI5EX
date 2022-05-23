776415cc8a2339c5cf3d1c5c0c45f47cba0331ac
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-23 15:24:44 +0900

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # imports
+from tabnanny import check
 import alarm
 import get_TH
 import disp
@@ -21,12 +22,14 @@ M = input("Minute:")
 
 # move
 while True:
+    print("chack =" + str(chack))
+    print("minute =" + M)
     times = get_time.get_time()
     THs = get_TH.get_TH(sensor)
     B = button.get_button(button_num)
 
     disp.set_disp(times, THs)
-    if times[3] == int(H) & times[4] == int(M):
+    if times[3] == int(H) and times[4] == int(M):
         chack += alarm.main_alarm(buzzer, B, count_Button)
         if chack >= 1:
             count_Button += 1
