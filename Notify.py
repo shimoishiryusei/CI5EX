f7e233225582f3736aef976de3a834f549878f92
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-23 15:15:50 +0900

    make notify

--- Notify.py
+++ Notify.py
@@ -12,10 +12,10 @@ def send_line_notify(notification_message):
     line_notify_api = 'https://notify-api.line.me/api/notify'
 
     headers = {
-        'Authorization': f'Bearer {line_notify_token}'
+        'Authorization': 'Bearer' +  line_notify_token
         }
     data = {
-        'message': f'message: {notification_message}'
+        'message': notification_message
         }
-        
+
     requests.post(line_notify_api, headers = headers, data = data)
