29b4c3166a2bb86c27bf60145d4c33b14cedba0f
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-23 14:25:17 +0900

    test

--- alarm.py
+++ alarm.py
@@ -5,20 +5,23 @@ def main_alarm(buzzer, B, count_B):
     Buzzzer_on = 0
     Buzzzer_off = 1
     grovepi.pinMode(buzzer, "OUTPUT")
-    while True:
-        try:
-            if B == 0 & count_B == 0:
+    try:
+        if B == 0:
+            if count_B == 0:
                 grovepi.digitalWrite(buzzer,1)
                 return Buzzzer_on
-                break
-
-            elif B == 1 | count_B >= 1:
+            
+            elif count_B != 0:
                 grovepi.digitalWrite(buzzer,0)
                 return Buzzzer_off
-                break
-                
-        except KeyboardInterrupt:
+            
+
+        elif B == 1:
             grovepi.digitalWrite(buzzer,0)
-            break
-        except IOError:
-            print("Error")
+            return Buzzzer_off
+        
+    except KeyboardInterrupt:
+        grovepi.digitalWrite(buzzer,0)
+    
+    except IOError:
+        print("Error")
