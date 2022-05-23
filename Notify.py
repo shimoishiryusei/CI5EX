e2a27352b8a70ceef2fee379c6124525da62090f
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-23 15:57:48 +0900

    test

--- Notify.py
+++ Notify.py
@@ -6,12 +6,13 @@ def send_line_notify():
     これはテストです．
     """
 
-    line_notify_token = 'dr8JswoCfScuCDn0r2WdS0yV2N1xmKmV1Glnw9esGVo'
+    line_notify_token = 'DiIYWZVqcwVqLrvbQsQwxAR0G0fSudp4QvntlOrkK1n'
     line_notify_api = 'https://notify-api.line.me/api/notify'
 
     headers = {
         'Authorization': "Bearer" +  line_notify_token
         }
+
     data = {
         'message': message
         }
