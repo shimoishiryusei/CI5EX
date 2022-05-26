3dd8f22af841ecf6518e8f4bbfcd7b8ca0db08a8
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-26 14:53:48 +0900

    make weather functino

--- Notify.py
+++ Notify.py
@@ -10,27 +10,29 @@ def send_line_notify():
     headers = {'Authorization': 'Bearer ' + access_token}
 
     #天気サイトから欲しい情報を取得する
-    url2 = "https://tenki.jp/forecast/3/17/4610/14100/"   #欲しい情報があるURLを指定
+    url2 = "https://tenki.jp/forecast/9/46/8610/43216/"   #欲しい情報があるURLを指定
     res = requests.get(url2)                              #上記URL情報を取得する
     soup = BeautifulSoup(res.content, 'html.parser')      #取得した情報をhtmlで解析する
 
     # 以下各種情報を取得
-    ddd = soup.find(class_="left-style").string                 
+    ddd = soup.find(class_="left-style").text                 
 
-    telop = soup.find("p", class_="weather-telop").string
+    telop = soup.find("p", class_="weather-telop").text
 
-    highlists = soup.find("dd",class_="high-temp temp").string
+    highlists = soup.find("dd",class_="high-temp temp").text
 
-    lowlists = soup.find("dd",class_="low-temp temp").string
+    lowlists = soup.find("dd",class_="low-temp temp").text
 
-    ttt = soup.find(class_="rain-probability").string
+    ttt = soup.find(class_="rain-probability")
 
     row=[]
     for t in ttt:
         row.append(t)
 
     # message変数に通知したい文を代入する　改行したい場合は "\n" とダブルクォテーションで囲う
-    message="\n" + ddd + "\n" + telop + "\n" + "最高　" + highlists + "\n" + "最低　" + lowlists + "\n"+ "---------" + "\n" +row[1].string +"\n" + "~6  : " + row[3].string + "\n" + "~12 : " + row[5].string +"\n" + "~18 : " + row[7].string +"\n" + "~24 : " + row[9].string
+    message="\n" + ddd + "\n" + telop + "\n" + "最高　" + highlists + "\n" + "最低　" + lowlists + "\n"+ "---------" + "\n" +row[1].text +"\n" + "~6  : " + row[3].text + "\n" + "~12 : " + row[5].text +"\n" + "~18 : " + row[7].text +"\n" + "~24 : " + row[9].text + "\n" + "おはようございます！" + "\n" + "今日も元気に頑張りましょう！"
 
     payload = {'message': message}
-    r = requests.post(url, headers=headers, params=payload,)
+    requests.post(url, headers=headers, params=payload,)
+
+send_line_notify()
