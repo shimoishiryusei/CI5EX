f86554fe804162db22bf1b1b03aa130994735ebf
shimoishiryusei <shimoishiryusei@gmail.com>
2022-05-26 14:09:44 +0900

    add bs4

--- Notify.py
+++ Notify.py
@@ -15,22 +15,22 @@ def send_line_notify():
     soup = BeautifulSoup(res.content, 'html.parser')      #取得した情報をhtmlで解析する
 
     # 以下各種情報を取得
-    ddd = soup.find(class_="left-style").text                 
+    ddd = soup.find(class_="left-style").string                 
 
-    telop = soup.find("p", class_="weather-telop").text
+    telop = soup.find("p", class_="weather-telop").string
 
-    highlists = soup.find("dd",class_="high-temp temp").text
+    highlists = soup.find("dd",class_="high-temp temp").string
 
-    lowlists = soup.find("dd",class_="low-temp temp").text
+    lowlists = soup.find("dd",class_="low-temp temp").string
 
-    ttt = soup.find(class_="rain-probability").text
+    ttt = soup.find(class_="rain-probability").string
 
     row=[]
     for t in ttt:
         row.append(t)
 
     # message変数に通知したい文を代入する　改行したい場合は "\n" とダブルクォテーションで囲う
-    message="\n" + ddd + "\n" + telop + "\n" + "最高　" + highlists + "\n" + "最低　" + lowlists + "\n"+ "---------" + "\n" +row[1].text +"\n" + "~6  : " + row[3].text + "\n" + "~12 : " + row[5].text +"\n" + "~18 : " + row[7].text +"\n" + "~24 : " + row[9].text
+    message="\n" + ddd + "\n" + telop + "\n" + "最高　" + highlists + "\n" + "最低　" + lowlists + "\n"+ "---------" + "\n" +row[1].string +"\n" + "~6  : " + row[3].string + "\n" + "~12 : " + row[5].string +"\n" + "~18 : " + row[7].string +"\n" + "~24 : " + row[9].string
 
     payload = {'message': message}
     r = requests.post(url, headers=headers, params=payload,)
