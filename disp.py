761e8a30b32ea12bd0dcd8d2f232271adb9c571d
shimoishiryusei <shimoishiryusei@gmail.com>
2022-06-16 15:27:23 +0900

    test

--- disp.py
+++ disp.py
@@ -6,10 +6,15 @@ import get_radian
 
 def set_disp(times, THs):
     Deg = get_radian.get_degree()
+    G_color = 255 - (Deg*0.255)
+    if G_color < 0:
+        G_color = 0
+    elif 255 < G_color:
+        G_color = 255
     Year,Month,Day,Hour,Minute,Second = times
     Tmp = THs[0]
     Hmd = THs[1]
     Time_strings = str(Month)+"/"+str(Day)+" "+str(Hour)+":"+str(Minute)+":"+str(Second) + "\n" + "T:" + str(Tmp) + " " + "H:" + str(Hmd)
     setText(Time_strings)
-    setRGB(255,255-(Deg*0.225),255)
+    setRGB(255,G_color,255)
     time.sleep(0.5)
