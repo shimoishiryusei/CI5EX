1df23c345baf4ea0f18fffd69ceaa96144653569
shimoishiryusei <shimoishiryusei@gmail.com>
2022-06-16 15:12:05 +0900

    test

--- get_radian.py
+++ get_radian.py
@@ -1,9 +1,13 @@
 import grovepi
 
 def get_degree():
+    potentiometer = 0
+
+    grovepi.pinMode(potentiometer, "INPUT")
+
     try:
         sensor_value = grovepi.analogRead(potentiometer)
         return sensor_value
-        
+
     except IOError:
         print("Error")
